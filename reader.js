ebf9ff9d286e377ec8f7d529bc16abd84c3b49a9
Dave Cramer <dauwhe@gmail.com>
2018-05-16 10:38:33 -0400

    initial commit

---
+++ reader.js
@@ -0,0 +1,192 @@
+
+// find manifest
+var manifestLink = document.querySelectorAll('link[rel=publication]')[0].href
+console.log(manifestLink);
+
+// create main element
+var mainFrame = document.createElement('main');
+
+  
+// create nav wrapper
+  
+   var newNav = document.createElement('nav');
+  newNav.setAttribute('role', 'doc-toc');
+  
+  document.getElementsByTagName('body')[0].appendChild(newNav);
+
+
+//fetch manifest
+
+let manifest = fetch(manifestLink).then(response => response.json()).then(response => response.spine);
+
+
+
+manifest.then(json => {
+
+  var newList = document.createElement('ol');
+  newNav.appendChild(newList);
+  console.log(newNav);
+  for (i=0; i < json.length; i++) {
+  var listItem = document.createElement('li');
+  var link = document.createElement('a');
+  listItem.appendChild(link);
+  link.setAttribute('href', json[i].href);
+  link.innerHTML = json[i].title;
+  newList.appendChild(listItem);
+  var iframe= document.createElement('iframe');
+  iframe.setAttribute('name', json[i].href);
+   iframe.setAttribute('id', json[i].href);
+    iframe.setAttribute('src', json[i].href);
+    mainFrame.appendChild(iframe);
+  };
+window.location.hash = json[0].href;
+});
+
+
+var tableOfContents = document.querySelectorAll('nav[role="doc-toc"]')[0];
+    tableOfContents.addEventListener("click", interceptClickEvent, false);
+
+   function showTOC() {
+    
+    
+    if (tableOfContents.style.display === "none") {
+        tableOfContents.style.display = "block";
+    } else {
+        tableOfContents.style.display = "none";
+    }
+    };
+
+// add iframes to body
+  
+    document.getElementsByTagName('body')[0].appendChild(mainFrame);
+    
+    
+    
+
+ function nextPage() {
+  console.log('clicked next page');
+  var currentTarget = window.location.hash.split('#')[1];
+  
+  
+  manifest.then(json => {
+  
+var current = json.map((el) => el.href).indexOf(currentTarget);
+
+
+window.location.hash = json[current + 1].href;
+
+})};
+
+
+ function prevPage() {
+  console.log('clicked previous page');
+  var currentTarget = window.location.hash.split('#')[1];
+  
+  
+  manifest.then(json => {
+  
+var current = json.map((el) => el.href).indexOf(currentTarget);
+
+
+window.location.hash = json[current - 1].href;
+
+})};
+
+
+var prevButton = document.createElement('button');
+prevButton.innerHTML = '&lt;';
+prevButton.setAttribute('id', 'prev');
+prevButton.setAttribute('onclick', 'prevPage();');
+prevButton.style.position = 'fixed';
+prevButton.style.top = '50%';
+prevButton.style.backgroundColor = 'rebeccapurple';
+prevButton.style.color = 'white';
+prevButton.style.fontWeight = 'bold';
+prevButton.style.fontSize = '30px';
+prevButton.style.borderStyle = 'solid';
+prevButton.style.borderColor = 'rebeccapurple';
+prevButton.style.paddingBottom = '44px';
+prevButton.style.paddingLeft = '0px';
+prevButton.style.left = '0';
+prevButton.style.borderTopRightRadius = '30px';
+prevButton.style.borderBottomRightRadius = '30px';
+prevButton.style.height = '30px';
+prevButton.style.width = '30px';
+document.body.prepend(prevButton);
+    
+
+  
+  var nextButton = document.createElement('button');
+nextButton.innerHTML = '&gt;';
+nextButton.setAttribute('id', 'next');
+nextButton.setAttribute('onclick', 'nextPage();');
+nextButton.style.position = 'fixed';
+nextButton.style.top = '50%';
+nextButton.style.backgroundColor = 'rebeccapurple';
+nextButton.style.color = 'white';
+nextButton.style.fontWeight = 'bold';
+nextButton.style.fontSize = '30px';
+nextButton.style.borderStyle = 'solid';
+nextButton.style.borderColor = 'rebeccapurple';
+nextButton.style.paddingBottom = '44px';
+nextButton.style.right = '0';
+nextButton.style.borderTopLeftRadius = '30px';
+nextButton.style.borderBottomLeftRadius = '30px';
+nextButton.style.height = '30px';
+nextButton.style.width = '30px';
+document.body.prepend(nextButton);
+
+
+var tocButton = document.createElement('button');
+tocButton.innerHTML = 'TOC';
+tocButton.setAttribute('id', 'toc-button');
+tocButton.setAttribute('onclick', 'showTOC();');
+tocButton.style.position = 'fixed';
+tocButton.style.top = '0';
+tocButton.style.backgroundColor = 'rebeccapurple';
+tocButton.style.color = 'white';
+tocButton.style.fontWeight = 'bold';
+tocButton.style.fontSize = '12px';
+tocButton.style.borderStyle = 'solid';
+tocButton.style.borderColor = 'rebeccapurple';
+tocButton.style.paddingBottom = '0px';
+tocButton.style.paddingRight = '12px';
+tocButton.style.left = '30px';
+tocButton.style.borderTopRightRadius = '30px';
+tocButton.style.borderBottomRightRadius = '30px';
+tocButton.style.borderTopLeftRadius = '30px';
+tocButton.style.borderBottomLeftRadius = '30px';
+tocButton.style.height = '30px';
+tocButton.style.width = '45px';
+document.body.prepend(tocButton);
+  
+ function interceptClickEvent(e) {
+    var href;
+    var target = e.target || e.srcElement;
+    if (target.tagName === 'A') {
+        href = target.getAttribute('href');
+
+
+   var linkTargetIframe = href.split('#')[0];
+   window.location.hash = linkTargetIframe;
+   console.log(linkTargetIframe);
+   
+   var newframe = document.getElementById(linkTargetIframe);
+   
+   console.log(href.split('#')[1]);
+
+//newframe.contentWindow.document.getElementById(href.split('#')[1]).scrollIntoView();
+
+
+
+
+   
+   
+        //put your logic here...
+        if (true) {
+
+           //tell the browser not to respond to the link click
+           e.preventDefault();
+        }
+    }
+}
